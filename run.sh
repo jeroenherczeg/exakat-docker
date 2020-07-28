4b917486b90b14ecccd0a67bb1096df2bc9c0f67
Jeroen Herczeg <jeroen@herczeg.be>
2020-07-29 00:12:12 +0200

    Simplfy and hope

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 url="https://api.apify.com/v2/key-value-stores/${APIFY_DEFAULT_KEY_VALUE_STORE_ID}/records/${APIFY_INPUT_KEY}"
 echo "$url"
-exakat init -p tmpprj -R https://github.com/sebastianbergmann/php-timer.git -v
+exakat init -p tmpprj -R https://github.com/jeroenherczeg/rector-test.git -v
 exakat project -p tmpprj -v
+ls /usr/src/exakat/tmpprj/
