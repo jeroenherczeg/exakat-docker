349d7c91fd9e5f072fca627f44b89e78be186570
Jeroen Herczeg <jeroen@herczeg.be>
2020-07-28 21:57:37 +0200

    Added test clone

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 url="https://api.apify.com/v2/key-value-stores/${APIFY_DEFAULT_KEY_VALUE_STORE_ID}/records/${APIFY_INPUT_KEY}"
 echo "$url"
+git clone https://github.com/sebastianbergmann/php-timer.git /usr/src/exakat/projects
 exakat doctor
