1b6a347f84c73b165fca47b5d2f679cba42158dd
Jeroen Herczeg <jeroen@herczeg.be>
2020-07-29 22:49:35 +0200

    More hope

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 URL="https://api.apify.com/v2/key-value-stores/${APIFY_DEFAULT_KEY_VALUE_STORE_ID}/records/${APIFY_INPUT_KEY}"
-SOURCE=$(curl -s "$URL" | php -r '$response = json_decode(file_get_contents("php://stdin"), true);echo $response["source"];')
-VERSION_ID=$(curl -s "$URL" | php -r '$response = json_decode(file_get_contents("php://stdin"), true);echo $response["version_id"];')
-echo "$VERSION_ID"
-exakat init -p tmpprj -R $SOURCE -v
+curl "$URL" -o /usr/src/input.json
+cat /usr/src/input.json
+SOURCE=$(php -r 'json_decode(file_get_contents("/usr/src/input.json"), true)["source"];')
+VERSION_ID=$(php -r 'json_decode(file_get_contents("/usr/src/input.json"), true)["version_id"];')
+echo "source: $SOURCE"
+echo "version_id: $VERSION_ID"
+exakat init -p tmpprj -R "$SOURCE" -v
 exakat project -p tmpprj -v
 cat /usr/src/exakat/projects/tmpprj/exakat.json
