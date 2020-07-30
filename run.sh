22d84a2d388fb140cb89da000d4fea052aec3442
Jeroen Herczeg <jeroen@herczeg.be>
2020-07-30 12:34:39 +0200

    echo var in php

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 URL="https://api.apify.com/v2/key-value-stores/${APIFY_DEFAULT_KEY_VALUE_STORE_ID}/records/${APIFY_INPUT_KEY}"
 curl -L -H "Accept: application/json" "$URL" -o /usr/src/input.json --compressed
 cat /usr/src/input.json
-SOURCE=$(php -r 'json_decode(file_get_contents("/usr/src/input.json"), true)["source"];')
-VERSION_ID=$(php -r 'json_decode(file_get_contents("/usr/src/input.json"), true)["version_id"];')
+SOURCE=$(php -r 'echo json_decode(file_get_contents("/usr/src/input.json"), true)["source"];')
+VERSION_ID=$(php -r 'echo json_decode(file_get_contents("/usr/src/input.json"), true)["version_id"];')
 echo "source: $SOURCE"
 echo "version_id: $VERSION_ID"
 exakat init -p tmpprj -R "$SOURCE" -v
